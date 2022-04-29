a607b2e688f166cbfeaf9f24b67a3c235fb425eb
aashqar <aashqar@atypon.com>
2022-04-30 01:20:48 +0300

    registeration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { Home, Login, Submit } from './views/index';
+import { Home, Login, Submit, Register } from './views/index';
 
 
 const App = () => {
@@ -10,9 +10,11 @@ const App = () => {
         <div>
           <nav>
             <Link to="/login" > Login </Link>
+            <Link to="/register" > Register </Link>
           </nav>
         </div>
         <Routes>
+        <Route exact path="/register" element = { <Register />} />
           <Route exact path="/home" element = { <Home />} />
           <Route exact path="/login" element={ <Login/> } />
           <Route exact path="/submit" element={ <Submit /> } />
