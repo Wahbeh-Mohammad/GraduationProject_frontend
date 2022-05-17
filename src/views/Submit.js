73de9e9afdca77331428ef742f8ee7af0ef976c8
aashqar <aashqar@atypon.com>
2022-05-17 11:54:30 +0300

    Editor

--- src/views/Submit.js
+++ src/views/Submit.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Cookies from "universal-cookie";
 import { prepareCode } from "../utils/encodingUtils";
+import Editor from "@monaco-editor/react"
 
 const SAMPLE_CPP = `
 #include <bits/stdc++.h>
@@ -14,48 +15,87 @@ int main() {
 }`
 
 const CPP_ID = 53;
-
+const languages = {
+    cpp: 53,
+    java: 62,
+    python: 71
+}
 const Submit = (props) => {
     const cookie = new Cookies();
     const [code, setCode] = useState(SAMPLE_CPP);
-    const [language, setLanguage] = useState(CPP_ID);
+    const [languageID, setLanguageID] = useState(53);
+    const [languageName, setLanguageName] = useState("cpp");
+    const [stdin, setStdin] = useState("");
+    const [stdout, setStdout] = useState("");
+    const [time, setTime] = useState(null);
+    const [memory, setMemory] = useState(null);
 
+    const hanldeLanguageChange = (e) => {
+        const name = e.target.value;
+        const id = languages[name];
+        console.log(name, id)
+        setLanguageName(name);
+        setLanguageID(id);
+    }
+    const handleStdin = (e) => {
+        setStdin(e.target.value)
+    }
     const handleSubmit = async () => {
-        const body = { source_code:prepareCode(code), language_id:language, problemId:"625c649fbfb5168e7f5dd371" }; 
+        const body = { stdin, source_code: prepareCode(code), language_id: languageID, problemId: "625c649fbfb5168e7f5dd371" };
         try {
             console.log(body);
-            const response = await fetch("http://localhost:3000/api/v1/submission/new", {
-                method:"POST",
+            const response = await fetch("http://localhost:3000/api/v1/submission/", {
+                method: "POST",
                 headers: {
-                    "Content-Type":"application/json",
-                    "authorization":`bearer ${cookie.get("jwt")}`
+                    "Content-Type": "application/json",
+                    "authorization": `bearer ${cookie.get("jwt")}`
                 },
                 body: JSON.stringify(body)
             });
             const parsedResponse = await response.json();
             console.log(parsedResponse);
+            setStdout(parsedResponse.data.stdout)
+            setTime(parsedResponse.data.time)
+            setMemory(parsedResponse.data.memory)
         } catch (err) {
             console.log(err);
         }
     }
 
     return (
-        <div> 
-            { cookie.get('jwt') && <>
-                    <div>
-                        <select value={language} onChange={setLanguage}>
-                            <option value={53}> C++ </option>
-                            <option value={62}> Java </option>
-                            <option value={71}> Python </option>
-                        </select>
-                    </div>
-                    <div>
-                        <textarea rows={50} cols={75} value={code} onChange={(e) => setCode(e.target.value)}/>
-                    </div>
-                    <div>
-                        <button onClick={ handleSubmit }> Submit </button>
-                    </div>
-                </>
+        <div>
+            {cookie.get('jwt') && <>
+                <div>
+                    <select value={languageName} onChange={hanldeLanguageChange} >
+                        <option value="cpp"> C++ </option>
+                        <option value="java"> Java </option>
+                        <option value="python"> Python </option>
+                    </select>
+                </div>
+                <Editor
+                    height="50vh"
+                    width="50vw"
+                    language={languageName}
+                    onChange={(e) => setCode(e)}
+                    className="editor"
+                />
+                <div >
+                    <label for = "input">Input:</label>
+                    <textarea name = "input" onChange={handleStdin} value={stdin} rows="10" cols="50"/>
+                    <label for = "output">Output:</label>
+                    <textarea name = "output" value={stdout} rows="10" cols="50">
+                        
+                        
+                    </textarea>
+                    <br />
+
+                    {memory && time && (<><span>Execution time: {time} s</span><br/>
+                    <span>Memory Usage: {memory} KB</span><br/></>)}
+                </div>
+                <div>
+                    <button onClick={handleSubmit}> Submit </button>
+                </div>
+            </>
             }
             {
                 !cookie.get('jwt') && <>
@@ -65,5 +105,5 @@ const Submit = (props) => {
         </div>
     );
 }
- 
+
 export default Submit;
