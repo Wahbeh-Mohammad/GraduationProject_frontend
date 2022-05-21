c04ee9c8fc784bacf9ad865c75b26b1960af46b9
abdalkreem <abdalkreemorabi11@gmail.com>
2022-05-21 09:14:05 -0900

    all subission

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
 import { Home, Login, Submit, Register,Problem, SubmitCode, CreateProblem , AllSubmision} from './views/index';
 import Navbar from './components/Navbar'
 
@@ -11,28 +10,15 @@ const App = () => {
     <div>
       <Navbar />
       <Router>
-<<<<<<< HEAD
-        <div>
-          <nav>
-            <Link to="/login" > Login </Link>
-            <Link to="/register" > Register </Link>
-            <Link to ="/allsubmision"> AllSubmision</Link>
-          </nav>
-        </div>
-=======
->>>>>>> main
         <Routes>
         <Route exact path="/register" element = { <Register />} />
           <Route exact path="/home" element = { <Home />} />
           <Route exact path="/login" element={ <Login/> } />
           <Route exact path="/submit" element={ <Submit /> } />
-<<<<<<< HEAD
           <Route exact path="/allsubmision" element={<AllSubmision />} />
-=======
           <Route exact path="/admin/problem/create" element={ <CreateProblem /> } />
           <Route exact path="/problem/:problemId" element={<Problem />} />
 
->>>>>>> main
         </Routes>
       </Router>
     </div>
