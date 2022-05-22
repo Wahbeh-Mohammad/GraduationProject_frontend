65be228f7cfed8556b54759adc0e72b881612807
aashqar <aashqar@atypon.com>
2022-05-22 16:45:56 +0300

    navbarcss

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Cookies from 'universal-cookie';
-
+import "../styles/navbar.css"
 const Navbar = () => {
     const cookie = new Cookies();
 
